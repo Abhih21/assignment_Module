4c17274cc596a3e11563b5bb77d86c3b0c471508
Abhishek Negi <112553331+Abhih21@users.noreply.github.com>
2023-09-09 22:29:08 +0530

    reversetheString.js

--- HOF assignment/reverseString/reversetheString.js
+++ HOF assignment/reverseString/reversetheString.js
@@ -1,27 +1,4 @@
 
-// // // let str = 'Input';
-// // function reverseString(str) {
-// //     let newString = " ";
-    
-// //     for(let i = str.length-1; i>=0; i--){
-// //         newString += str[i];
-        
-// //     }
-// //     // console.log(newString);
-// //     return newString;
-// // }
-// // setTimeout(reverseString, 2000);
-// // console.log(reverseString("Input"));
-
-// const d = setTimeout(reverseStr, 2000);
-
-// function reverseStr() {
-
-//     const str ="Hello";
-//     const reversedStr = str.split('').reverse().join('');
-//     console.log(reversedStr);
-// }
-
 const d = setTimeout(() => {
     
     const Input ="Hello";
